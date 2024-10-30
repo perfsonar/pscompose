c5fc93fec27b9f067c1e4518f46e0f628c14ab11
James Kafader <jkafader@es.net>
2024-10-29 21:14:07 -0700

    in-progress version of HTTP Basic Auth API endpoints

--- pscompose/models.py
+++ pscompose/models.py
@@ -1,1 +1,57 @@
-# write a models.py
+from typing import Dict, List, Any, Optional
+from pydantic import BaseModel as PydanticValidationModel
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.dialects import postgresql
+from enum import Enum
+
+from sqlalchemy import create_engine, Column, Integer, Text
+
+from pscompose.settings import POSTGRES_DB_NAME, TOKEN_SCOPES
+
+# Pydantic Validation Model subclasses will model input and output types from the API.
+# SQLAlchemy Storage subclasses will model data as stored in the DB.
+
+engine = create_engine(
+    "postgresql:///" + POSTGRES_DB_NAME
+)
+
+SQLAlchemyStorage = declarative_base()
+
+class ScopeEnum(str, Enum):
+    read = TOKEN_SCOPES["read"]
+    write = TOKEN_SCOPES["write"]
+    publish = TOKEN_SCOPES["publish"]
+    admin = TOKEN_SCOPES["admin"]
+
+
+class UserUpdate(PydanticValidationModel):
+    '''User Update models the data needed for the situation where a User receives an update.'''
+    username: str
+    name: str
+    scopes: List[ScopeEnum]
+
+
+class UserCreate(UserUpdate):
+    '''User Create models the data needed for the situation where a User is created from scratch. Includes "password."'''
+    password: str
+
+
+class User(UserUpdate):
+    '''User models user data as returned by the API.'''
+    pass
+
+
+class PasswordReset(PydanticValidationModel):
+    '''Password Reset just validates that a user password has been sent.'''
+    password: str
+
+
+class UserTable(SQLAlchemyStorage):
+    '''The UserTable model defines what's actually stored in the Postgres 'user' table.'''
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    # username used to store both email and username
+    username = Column(Text)
+    name = Column(Text)
+    password = Column(Text)
+    scopes = Column(postgresql.JSON)
--- pscompose/settings.py
+++ pscompose/settings.py
@@ -1,1 +1,33 @@
-# write a settings.py module
+import yaml
+import os
+import sys
+import random
+import string
+
+DEFAULT_CONFIG_FILE = "/etc/pscompose/settings.yml"
+
+CONFIG_FILENAME = os.environ.get("PSCOMPOSE_SETTINGS", DEFAULT_CONFIG_FILE)
+LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
+
+conf = None
+try:
+    with open(CONFIG_FILENAME) as f:
+        conf = yaml.safe_load(f)
+except Exception as e:
+    print(f"Unable to read config file {CONFIG_FILENAME} -- {e}")
+    sys.exit(1)
+
+if conf is None:
+    conf = {}
+
+POSTGRES = conf.get("POSTGRES", {})  # default to empty dict so we can set further defaults
+AUTH = conf.get("auth", {})
+
+POSTGRES_DB_NAME = POSTGRES.get("DB_NAME", "pscompose")
+
+TOKEN_SCOPES = {
+    "read": AUTH.get("read_scope", "pscompose:read"),
+    "write": AUTH.get("write_scope", "pscompose:write"),
+    "publish": AUTH.get("publish_scope", "pscompose:publish"),
+    "admin": AUTH.get("admin_scope", "pscompose:admin"),
+}
