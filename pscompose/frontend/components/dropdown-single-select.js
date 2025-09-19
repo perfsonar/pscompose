93db271ad221c3a9efe7e7431e12a5a8c840b292
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-09-19 12:06:19 -0400

    customize dropdown to align with design system

--- pscompose/frontend/components/dropdown-single-select.js
+++ pscompose/frontend/components/dropdown-single-select.js
@@ -14,13 +14,44 @@ export class SingleSelectDropdown extends HTMLElement {
         this.render();
     }
 
+    attachToggleDropdown() {
+        const options = this.querySelector(".options");
+        if (!options) return;
+        this.querySelector(".select").addEventListener("click", () => {
+            options.classList.toggle("open");
+            this.querySelector(".dropdown").classList.toggle("active");
+            this.attachOptionListeners();
+        });
+    }
+
+    closeDropdown() {
+        const dropdown = this.querySelector(".options");
+        if (dropdown) {
+            options.classList.remove("open");
+            this.querySelector(".dropdown").classList.remove("active");
+        }
+    }
+
+    attachOptionListeners() {
+        document.querySelectorAll(".option").forEach((item) => {
+            item.onclick = () =>
+                this.selectOption(item.getAttribute("data-value"), item.textContent);
+        });
+    }
+
+    selectOption(value, title) {
+        this.setAttribute("selected", value);
+        this.render();
+        this.dispatchEvent(new Event("change", { bubbles: true }));
+        this.closeDropdown();
+    }
+
     render() {
-        const options = this.options ? JSON.parse(this.options) : [];
-        const optionsHTML = options
-            ? options
-                  .map((option) => `<option value="${option.const}">${option.title}</option>a`)
-                  .join("")
-            : "";
+        const options = this.getAttribute("options")
+            ? JSON.parse(this.getAttribute("options"))
+            : [];
+        const selectedValue = this.getAttribute("selected") || "";
+        const selectedOption = options ? options.find((opt) => opt.const === selectedValue) : null;
 
         this.innerHTML = `
             <div class="container">
@@ -34,22 +65,42 @@ export class SingleSelectDropdown extends HTMLElement {
                             : ""
                     }
                 </label>
-                <select>
-                    <option value="">Choose ${this.getAttribute("label")}</option>
-                    ${optionsHTML}
-                </select>
+                <div class="dropdown">
+                    <div class="select">
+                        ${
+                            selectedOption
+                                ? selectedOption.title
+                                : `<p style="color: var(--copyAlt-color)">Select ${this.getAttribute(
+                                      "label",
+                                  )}</p>`
+                        }
+                        <web-button id="down-btn" type="button" data-righticon="chevron-down" data-theme="Icon"></web-button>
+                    </div>
+                    <ul class="options">
+                        ${
+                            options
+                                ? options
+                                      .map(
+                                          (option) => `
+                            <li ${
+                                option.const == selectedValue
+                                    ? 'class="option active"'
+                                    : 'class="option"'
+                            }
+                            data-value="${option.const}">
+                            ${option.title}
+                            </li>
+                        `,
+                                      )
+                                      .join("")
+                                : ""
+                        }
+                    </ul>
+                </div>
                 ${this.getAttribute("required") == "true" ? `<required>Required<required>` : ""}
             </div>
         `;
-
-        if (this.getAttribute("selected")) {
-            this.querySelector("select").value = this.getAttribute("selected");
-        }
-
-        this.querySelector("select").addEventListener("change", (event) => {
-            this.selected = event.target.value;
-            this.dispatchEvent(new Event("change", { bubbles: true }));
-        });
+        this.attachToggleDropdown();
     }
 }
 
