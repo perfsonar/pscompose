5582905415a495a56e1406da797bc3d7eb6fb2ff
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-09-23 13:23:01 -0400

    create web-modal component and update modal popup logic so it's not relying on htmx-confirm and ajax requests

--- pscompose/frontend/components/web-button.js
+++ pscompose/frontend/components/web-button.js
@@ -7,6 +7,7 @@ export class WebButton extends HTMLElement {
         "data-lefticon",
         "data-righticon",
         "data-link",
+        "data-modalconfirm",
     ];
     passthroughAttributes = {};
     passthroughAttributeMatchers = [new RegExp(/hx-.*/), new RegExp(/data-.*/)];
@@ -35,9 +36,15 @@ export class WebButton extends HTMLElement {
     }
 
     render() {
+        const hasModalConfirm = !!this.getAttribute("data-modalconfirm");
+        const hxAttrs = Array.from(this.attributes)
+            .filter((attr) => attr.name.startsWith("hx-"))
+            .map((attr) => `${attr.name}="${attr.value}"`)
+            .join(" ");
+
         this.innerHTML = `
             ${
-                this.getAttribute("data-link")
+                this.getAttribute("data-link") && !hasModalConfirm
                     ? `<a href="${this.getAttribute("data-link")}" style="text-decoration: none;">`
                     : ""
             }
@@ -47,15 +54,13 @@ export class WebButton extends HTMLElement {
                 
                 ${
                     this.getAttribute("data-lefticon")
-                        ? `<i data-lucide="${this.getAttribute("data-lefticon",
-                          )}"></i>`
+                        ? `<i data-lucide="${this.getAttribute("data-lefticon")}"></i>`
                         : ""
                 }
                 ${this.getAttribute("data-label") || ""}
                 ${
                     this.getAttribute("data-righticon")
-                        ? `<i data-lucide="${this.getAttribute("data-righticon",
-                          )}"></i>`
+                        ? `<i data-lucide="${this.getAttribute("data-righticon")}"></i>`
                         : ""
                 }
             </button>
@@ -64,11 +69,45 @@ export class WebButton extends HTMLElement {
 
         let btn = this.querySelector("button");
         Object.keys(this.passthroughAttributes).forEach((k) => {
-            btn.setAttribute(k, this.passthroughAttributes[k]);
             if (k.startsWith("hx-")) {
+                if (!hasModalConfirm) {
+                    btn.setAttribute(k, this.passthroughAttributes[k]);
+                }
                 this.removeAttribute(k);
             }
         });
+
+        if (this.getAttribute("data-modalconfirm")) {
+            btn.addEventListener("click", () => {
+                this.modalInnerHTML = `<web-modal
+                        ${
+                            this.getAttribute("data-link")
+                                ? `link=${this.getAttribute("data-link")}`
+                                : ""
+                        }
+                        ${
+                            this.getAttribute("data-righticon")
+                                ? `icon=${this.getAttribute("data-righticon")}`
+                                : ""
+                        }
+                        ${
+                            this.getAttribute("data-lefticon")
+                                ? `icon=${this.getAttribute("data-lefticon")}`
+                                : ""
+                        }
+
+                        theme="${this.getAttribute("data-theme")}"
+                        message="${this.getAttribute("data-modalconfirm")}"
+                        confirm-label = "${this.getAttribute("data-label")}"
+                        ${this.hxAttrs}
+                    ></web-modal>`;
+
+                const responseContainer = document.getElementById("response-container");
+                if (responseContainer) {
+                    responseContainer.innerHTML = this.modalInnerHTML;
+                }
+            });
+        }
     }
 }
 
---
+++ pscompose/frontend/components/web-modal.js
@@ -0,0 +1,84 @@
+export class WebModal extends HTMLElement {
+    static observedAttributes = ["confirm-label", "link", "theme", "message"];
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this[name] = newValue;
+        this.render();
+    }
+
+    attachListeners() {
+        const yes = document.querySelector("#confirm-yes");
+        const no = document.querySelector("#confirm-no");
+        const underlay = document.querySelector(".modal-underlay");
+
+        yes.onclick = function () {
+            document.getElementById("confirm-modal").style.display = "none";
+            const confirmEvent = new CustomEvent("confirm-yes-clicked", {
+                bubbles: true,
+            });
+            document.body.dispatchEvent(confirmEvent);
+        };
+        no.onclick = function () {
+            document.getElementById("confirm-modal").style.display = "none";
+        };
+        underlay.onclick = function () {
+            document.getElementById("confirm-modal").style.display = "none";
+        };
+    }
+
+    render() {
+        let accentColor = "var(--copy-color)";
+        if (this.getAttribute("theme")) {
+            if (this.getAttribute("theme") == "Secondary") {
+                accentColor = "var(--success-color)";
+            }
+            if (this.getAttribute("theme") == "Error") {
+                accentColor = "var(--error-color)";
+            }
+            if (this.getAttribute("theme") == "Warning") {
+                accentColor = "var(--warning-color)";
+            }
+        }
+
+        this.innerHTML = `
+            <div id="confirm-modal">
+                <div class="modal-underlay"></div>
+
+                <div class="modal-content" style="border-color:${accentColor}" >
+                    <i  style="width: 2rem; height: 2rem; color: ${accentColor}" data-lucide=  "alert-triangle">
+                    </i>
+                    <h4 id="confirm-question">${this.getAttribute("message")}</h4>
+                    <div class="save-cancel">
+                        <web-button id="confirm-no" data-label="Cancel" data-theme="Shadow"></web-button>
+                        <web-button
+                            id="confirm-yes"
+                            ${
+                                this.getAttribute("link")
+                                    ? `data-link=${this.getAttribute("link")}`
+                                    : ""
+                            }
+                            ${
+                                this.getAttribute("icon")
+                                    ? `data-righticon=${this.getAttribute("icon")}`
+                                    : ""
+                            }
+                            data-label=${this.getAttribute("confirm-label")}
+                            data-theme=${this.getAttribute("theme")}
+                        ></web-button>
+                    </div>
+                </div>
+            </div>
+        `;
+        this.attachListeners();
+    }
+}
+
+customElements.define("web-modal", WebModal);
