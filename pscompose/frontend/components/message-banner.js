8be7935c9699ae222240331d6a7d4e18ab16f678
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-10-06 12:12:01 -0400

    add themes for modal popup

--- pscompose/frontend/components/message-banner.js
+++ pscompose/frontend/components/message-banner.js
@@ -1,30 +1,41 @@
-function processLastMessage(responseContainer, displayDuration = 3000) {
+function processLastMessage() {
     let key = "confirmMessage";
-    let confirmMsg, confirmCss;
+    let confirmMsg, confirmTheme;
     if (sessionStorage.getItem(key)) {
         let confirm = JSON.parse(sessionStorage.getItem(key));
         confirmMsg = confirm[0];
-        confirmCss = confirm[1];
+        confirmTheme = confirm[1];
     }
-    const container = document.querySelector(responseContainer);
+    const container = document.querySelector("#response-container");
 
     function showNext() {
         if (!confirmMsg) {
             container.classList.remove("show");
             return;
         }
-        container.innerHTML = `<i data-lucide="check-circle-2"></i> 
-                                <h6>${confirmMsg}</h6>`;
-        container.style.backgroundColor = confirmCss;
+
+        if (confirmTheme == "Success") {
+            container.innerHTML = `<i data-lucide="check-circle-2"></i> 
+                                    <h6>${confirmMsg}</h6>`;
+            container.style.backgroundColor = `rgba(var(--success-color-rgb), 0.5)`;
+        } else if (confirmTheme == "Warning") {
+            container.innerHTML = `<i data-lucide="alert-triangle"></i> 
+                                    <h6>${confirmMsg}</h6>`;
+            container.style.backgroundColor = `rgba(var(--warning-color-rgb), 0.5)`;
+        } else if (confirmTheme == "Error") {
+            container.innerHTML = `<i data-lucide="circle-x"></i> 
+                                    <h6>${confirmMsg}</h6>`;
+            container.style.backgroundColor = `rgba(var(--error-color-rgb), 0.5)`;
+        }
         container.classList.add("show");
         sessionStorage.removeItem(key);
         setTimeout(() => {
             container.classList.remove("show");
-        }, displayDuration);
+        }, 3000);
     }
     if (confirmMsg) showNext();
 }
 
 window.addEventListener("load", () => {
-    processLastMessage("#response-container", 3000);
+    processLastMessage();
 });
