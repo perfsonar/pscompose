387bc577e0b9f925a328420ba6d484feefcdc258
sartaj10 <ssbaveja@es.net>
2025-10-07 15:39:34 -0700

    Work on adding schedule, archive forms

--- pscompose/form_schemas.py
+++ pscompose/form_schemas.py
@@ -4,7 +4,7 @@
 # TODO: Should the properties be named as how they appear in the config JSON?
 # Eg: using display-name instead of name
 ADDRESS_SCHEMA = {
-    "title": "Schema for creating a new host form",
+    "title": "Schema for creating a new address",
     "type": "object",
     "properties": {
         "name": {
@@ -44,6 +44,8 @@ ADDRESS_SCHEMA = {
         "contexts": {
             "type": "array",
             "title": "Contexts",
+            # TODO: Dynamically populate the enum of IDs and labels
+            # "items": {"oneOf": []},
             "items": {
                 "oneOf": [
                     {"const": "male", "title": "Male"},
@@ -157,13 +159,7 @@ GROUP_SCHEMA = {
                         "items": {"oneOf": []},
                     },
                     "excludes-self": {"type": "boolean", "title": "Excludes Self"},
-                    "excludes": {
-                        "type": "array",
-                        "title": "Excludes",
-                        "items": {
-                            "oneOf": []
-                        }
-                    },
+                    "excludes": {"type": "array", "title": "Excludes", "items": {"oneOf": []}},
                     "_meta": {
                         "type": "string",
                         "title": "Other Meta",
@@ -181,13 +177,7 @@ GROUP_SCHEMA = {
                     "type": {"const": "mesh"},
                     "addresses": {"type": "array", "title": "Addresses", "items": {"oneOf": []}},
                     "excludes-self": {"type": "boolean", "title": "Excludes Self"},
-                    "excludes": {
-                        "type": "array",
-                        "title": "Excludes",
-                        "items": {
-                            "oneOf": []
-                        }
-                    },
+                    "excludes": {"type": "array", "title": "Excludes", "items": {"oneOf": []}},
                     "_meta": {
                         "type": "string",
                         "title": "Other Meta",
@@ -257,6 +247,199 @@ GROUP_UI_SCHEMA = {
                 {"type": "Control", "scope": "#/properties/excludes"},
                 {"type": "Control", "scope": "#/properties/_meta", "options": {"multi": True}},
             ],
+        },
+    ],
+}
+
+SCHEDULE_SCHEMA = {
+    "title": "Schema for creating a new schedule",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string",
+            "title": "Schedule Name",
+            "description": "A string to identify this schedule",
+            "default": "",
+        },
+        "slip": {
+            "type": "string",
+            "title": "Slip",
+            "description": "ISO8601 DURATION that allows the start of each run to be as much as the DURATION later than their ideal scheduled time. Default is 5 minutes (PT5M)",
+            "default": "PT5M",
+        },
+        "sliprand": {
+            "type": "boolean",
+            "title": "Randomize Slip",
+            "description": "Randomly choose a timeslot within the allowed slip instead of choosing earliest available",
+            "default": True,
+        },
+        "repeat": {
+            "type": "string",
+            "title": "Repeat",
+            "description": "ISO8601 duration that repeats runs at the specified duration",
+        },
+        # "start": {
+        #     "type": "string",
+        #     "title": "Start",
+        #     "description": "",
+        # },
+        # "until": {
+        #     "type": "string",
+        #     "title": "Until",
+        #     "description": "",
+        # },
+        "max-runs": {
+            "type": "integer",
+            "title": "Max Runs",
+            "description": "Allow the task to run up to N times",
+            "minimum": 1,
+        },
+        "_meta": {
+            "type": "string",
+            "title": "Other Meta",
+            "description": "Fill in information such as display-name and display-set as an object",
+            "default": "",
+        },
+    },
+    "required": [
+        "name",
+    ],
+    "renderers": {},
+}
+
+SCHEDULE_UI_SCHEMA = {
+    "type": "VerticalLayout",
+    "elements": [
+        {
+            "type": "Control",
+            "scope": "#/properties/name",
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/repeat",
+        },
+        {
+            "type": "HorizontalLayout",
+            "elements": [
+                {"type": "Control", "scope": "#/properties/slip"},
+                {
+                    "type": "Control",
+                    "scope": "#/properties/sliprand",
+                },
+            ],
+        },
+        # {
+        #     "type": "Control",
+        #     "scope": "#/properties/start",
+        # },
+        # {
+        #     "type": "Control",
+        #     "scope": "#/properties/until",
+        # },
+        {
+            "type": "Control",
+            "scope": "#/properties/max-runs",
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/_meta",
+        },
+    ],
+}
+
+ARCHIVE_SCHEMA = {
+    "title": "Schema for creating a new archive",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string",
+            "title": "Archive Name",
+            "description": "A string to identify this archive",
+            "default": "",
+        },
+        # TODO: Make this a dropdown with options fetched from pScheduler
+        "archiver": {
+            "type": "string",
+            "title": "Archiver",
+            "description": "A string that type of archive",
+            "default": "",
+        },
+        "data": {
+            "type": "string",
+            "title": "Data",
+            "description": "JSON object that specifies archive-specific parameters. Archive objects in pSConfig are taken directly from pScheduler. Eg: _url",
+            "default": "",
+        },
+        "schema": {
+            "type": "integer",
+            "title": "Schema Version",
+            "description": "The schema version number",
+        },
+        "ttl": {
+            "type": "string",
+            "title": "TTL",
+            "description": "ISO8601 duration that specifies the ttl",
+            "default": "",
+        },
+        "label": {
+            "type": "string",
+            "title": "Label",
+            "description": "",
+        },
+        "transform": {
+            "type": "string",
+            "title": "Transform",
+            "description": "",
+            "default": "",
+        },
+        "_meta": {
+            "type": "string",
+            "title": "Other Meta",
+            "description": "Fill in information such as display-name and display-set as an object",
+            "default": "",
+        },
+    },
+    "required": [
+        "name",
+        "archiver",
+        "data",
+    ],
+    "renderers": {},
+}
+
+ARCHIVE_UI_SCHEMA = {
+    "type": "VerticalLayout",
+    "elements": [
+        {
+            "type": "Control",
+            "scope": "#/properties/name",
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/archiver",
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/data",
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/label",
+        },
+        {
+            "type": "HorizontalLayout",
+            "elements": [
+                {"type": "Control", "scope": "#/properties/schema"},
+                {"type": "Control", "scope": "#/properties/ttl"},
+            ],
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/transform",
+        },
+        {
+            "type": "Control",
+            "scope": "#/properties/_meta",
         },
     ],
 }
