dcd39ee260b8f2a095300f63975fc0ac627c6dc0
sartaj10 <ssbaveja@es.net>
2025-10-13 20:42:08 -0700

    Setup task forms

--- pscompose/api/routers/tasks.py
+++ pscompose/api/routers/tasks.py
@@ -3,9 +3,9 @@ from fastapi_versioning import version
 from fastapi.responses import JSONResponse
 
 from pscompose.settings import DataTypes
-from pscompose.utils import generate_router
+from pscompose.utils import generate_router, enrich_schema
 from pscompose.backends.postgres import backend
-from pscompose.form_schemas import GROUP_SCHEMA, GROUP_UI_SCHEMA
+from pscompose.form_schemas import TASK_SCHEMA, TASK_UI_SCHEMA
 
 # Setup CRUD endpoints
 router = generate_router("task")
@@ -15,7 +15,48 @@ router = generate_router("task")
 @router.get("/api/task/new/form", summary="Return the new form to be rendered")
 @version(1)
 def get_new_form():
-    payload = {"ui_schema": GROUP_UI_SCHEMA, "json_schema": GROUP_SCHEMA, "form_data": {}}
+    try:
+        groups = backend.get_results(datatype=DataTypes.GROUP)
+        tests = backend.get_results(datatype=DataTypes.TEST)
+        schedules = backend.get_results(datatype=DataTypes.SCHEDULE)
+        archives = backend.get_results(datatype=DataTypes.ARCHIVE)
+        tools = backend.get_results(datatype="tool")  # TODO: Get this from pScheduler
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to fetch data: {str(e)}")
+
+    enriched_schema = enrich_schema(
+        base_schema=TASK_SCHEMA,
+        properties=["group"],
+        rows=groups,
+    )
+
+    enriched_schema = enrich_schema(
+        base_schema=enriched_schema,
+        properties=["test"],
+        rows=tests,
+    )
+
+    enriched_schema = enrich_schema(
+        base_schema=enriched_schema,
+        properties=["schedule"],
+        rows=schedules,
+    )
+
+    enriched_schema = enrich_schema(
+        base_schema=enriched_schema,
+        properties=["archives"],
+        rows=archives,
+    )
+
+    enriched_schema = enrich_schema(
+        base_schema=enriched_schema,
+        properties=["tools"],
+        rows=tools,
+    )
+
+    print("enriched_schema:", enriched_schema)
+
+    payload = {"ui_schema": TASK_UI_SCHEMA, "json_schema": enriched_schema, "form_data": {}}
     return JSONResponse(content=payload)
 
 
@@ -33,8 +74,8 @@ def get_existing_form(item_id: str):
         raise HTTPException(status_code=404, detail=f"Task with id: {item_id} not found")
 
     payload = {
-        "ui_schema": GROUP_UI_SCHEMA,
-        "json_schema": GROUP_SCHEMA,
+        "ui_schema": TASK_UI_SCHEMA,
+        "json_schema": TASK_SCHEMA,
         "form_data": response_json,
     }
     return JSONResponse(content=payload)
