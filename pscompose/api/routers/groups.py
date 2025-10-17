a075ed5b7ac82597a2bdb838c6dda740f3af1c47
sartaj10 <ssbaveja@es.net>
2025-10-17 15:10:03 -0700

    Update groups API

--- pscompose/api/routers/groups.py
+++ pscompose/api/routers/groups.py
@@ -49,10 +49,11 @@ def sanitize_data(data):
 
     ref_set = data["ref_set"]
     for key in ("addresses", "a-addresses", "b-addresses"):
-        for address in json_data.get(key, []):
-            name = address["name"]
-            if name not in ref_set:
-                ref_set.append(name)
+        if json_data.get(key) is not None:
+            for address in json_data.get(key, []):
+                name = address["name"]
+                if name not in ref_set:
+                    ref_set.append(name)
 
     data["ref_set"] = ref_set
     return data
