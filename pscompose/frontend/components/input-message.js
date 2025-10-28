29607a6d81b50b9225054a8c329599d8aec4424d
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-10-28 11:03:25 -0400

    clean up input message

--- pscompose/frontend/components/input-message.js
+++ pscompose/frontend/components/input-message.js
@@ -15,17 +15,19 @@ export class InputMessage extends HTMLElement {
     }
 
     render() {
-        const errorExist = this.getAttribute("errors") ? true : false;
-        const errorRequired = this.getAttribute("errors") == "is a required property";
-        const errorState = errorExist & !errorRequired;
-        const required = JSON.parse(this.getAttribute("required")) ? true : false;
+        const error = this.getAttribute("errors");
+        const required = JSON.parse(this.getAttribute("required") || "false");
+
+        const errorExist = error && error.trim() !== "";
+        const errorRequired = error !== "is a required property";
+        const errorState = errorExist && errorRequired;
 
         this.innerHTML = `                
-            <div class="input-message ${errorState ? "error" : ""}" >
-                <div class="errors">${errorExist ? `${this.getAttribute("errors")}` : ""}</div>
-                <div class="required">${required ? `Required` : ""}</div>
-            </div>
-        `;
+        <div class="input-message ${errorState ? "error" : ""}">
+            <div class="errors">${error || ""}</div>
+            <div class="required">${required ? "Required" : ""}</div>
+        </div>
+    `;
     }
 }
 
