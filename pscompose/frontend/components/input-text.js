c17c67ece0704af33e4e43f676f8f14b46e6498f
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-10-30 11:53:04 -0400

    update observed attr for input checkbox

--- pscompose/frontend/components/input-text.js
+++ pscompose/frontend/components/input-text.js
@@ -1,5 +1,5 @@
 export class InputText extends HTMLElement {
-    static observedAttributes = ["label", "value"];
+    static observedAttributes = ["label", "value", "description", "required", "errors"];
 
     constructor() {
         super();
