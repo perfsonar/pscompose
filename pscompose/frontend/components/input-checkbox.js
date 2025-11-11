0602d81f9dc41b1829125c538d128c82b63d288b
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-11-11 11:47:32 -0500

    update input checkbox to extend input text

--- pscompose/frontend/components/input-checkbox.js
+++ pscompose/frontend/components/input-checkbox.js
@@ -1,54 +1,15 @@
-class InputCheckbox extends HTMLElement {
-    static observedAttributes = ["label", "value", "errors", "description", "required"];
+import { InputText } from "./input-text.js";
 
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        this.render();
-        lucide.createIcons();
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        this[name] = newValue;
-        this.render();
-        lucide.createIcons();
-    }
-
+class InputCheckbox extends InputText {
     render() {
-        const desc = this.getAttribute("description");
-        const descAttr = desc != null ? ` desc='${desc}'` : "";
-
-        this.innerHTML = `
-            <div class="container">
-                <div class="input-checkbox-wrapper">
-                    <input type="checkbox" ${
-                        JSON.parse(this.getAttribute("value")) === true ? "checked" : ""
-                    } >
-                    </input>
-                <input-label label='${this.getAttribute("label")}'${descAttr}></input-label>
-                </div>
-                <input-message errors='${this.getAttribute(
-                    "errors",
-                )}' required='${this.getAttribute("required")}'></input-message>
-            </div>
-
-            <div class="checkbox-container-disabled">
-                <label>${this.getAttribute("label")}</label> 
-                <div class="wrapper">       
-                    <p> ${JSON.parse(this.getAttribute("value"))}</p>
-                </div>
-            </div>
-        `;
-
-        const input = this.querySelector("input");
-        input.addEventListener("change", (event) => {
-            event.stopPropagation();
-            const boolVal = event.target.checked ? true : false;
-            this.setAttribute("value", JSON.stringify(boolVal));
-            this.dispatchEvent(new Event("change", { bubbles: true }));
-        });
+        super.render();
+        this.inputEl.checked = this.value === "true" || this.value === true;
+        if (!this.disabled) {
+            this.inputEl?.setAttribute("type", "checkbox");
+            this.inputEl?.removeAttribute("value");
+        } else {
+            this.inputEl.value = this.value;
+        }
     }
 }
 
