9d19a5e0fd17457ffb8d8b04fc7b154868808c46
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-11-11 11:28:31 -0500

    add form-control component to clean up components

---
+++ pscompose/frontend/components/form-control.js
@@ -0,0 +1,79 @@
+export class FormControl extends HTMLElement {
+    static get observedAttributes() {
+        return ["class", "label", "value", "description", "error", "required", "disabled"];
+    }
+
+    constructor() {
+        super();
+        this.slotEl = "";
+    }
+
+    get value() {
+        return this.getAttribute("value") ? JSON.parse(this.getAttribute("value")) : undefined;
+    }
+    set value(v) {
+        this.setAttribute("value", JSON.stringify(v) ?? "");
+    }
+
+    get label() {
+        return this.getAttribute("label") ?? "";
+    }
+    set label(v) {
+        this.setAttribute("label", v ?? "");
+    }
+
+    get description() {
+        return this.getAttribute("description") ?? "";
+    }
+    set description(v) {
+        this.setAttribute("description", v ?? "");
+    }
+
+    get error() {
+        return this.getAttribute("error") ?? "";
+    }
+    set error(v) {
+        this.setAttribute("error", v ?? "");
+    }
+
+    get disabled() {
+        return this.hasAttribute("disabled");
+    }
+    set disabled(v) {
+        v ? this.setAttribute("disabled", "") : this.removeAttribute("disabled");
+    }
+
+    get required() {
+        return this.hasAttribute("required");
+    }
+    set required(v) {
+        v ? this.setAttribute("required", "") : this.removeAttribute("required");
+    }
+
+    connectedCallback() {
+        this.renderControl();
+        this.render();
+    }
+
+    attributeChangedCallback() {
+        this.renderControl();
+        this.render();
+    }
+
+    renderControl() {
+        this.innerHTML = `
+            <div class="container">
+                <input-label label="${this.label}" desc="${this.description}"></input-label>
+                ${this.slotEl || ""}
+                <input-message error="${this.error}" ${
+                    this.required ? "required" : ""
+                }></input-message>
+            </div>
+        `;
+        this.containerEl = this.querySelector(".container");
+    }
+
+    render() {}
+}
+
+customElements.define("form-control", FormControl);
