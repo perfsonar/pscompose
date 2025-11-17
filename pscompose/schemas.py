e5fb5132d23e618707378fcfaa5c71904f94bc0a
Johanna Lee <123130134+johyunjihyunji@users.noreply.github.com>
2025-11-17 10:39:45 -0500

    schema.py to include leadbindaddress, pscheduler address variable type

--- pscompose/schemas.py
+++ pscompose/schemas.py
@@ -50,6 +50,10 @@ class HostNamePort(BaseModel):
     )
 
 
+class HostNameVar(BaseModel):
+    __root__: constr(regex=r"^\{% [a-zA-Z_][a-zA-Z0-9_]*(\[\d+\])? %}$")
+
+
 class IPAddress(BaseModel):
     __root__: Union[IPv4Address, IPv6Address]
 
@@ -67,7 +71,7 @@ class Timestamp(BaseModel):
 
 
 class Host(BaseModel):
-    __root__: Union[HostName, IPAddress]
+    __root__: Union[HostName, HostNameVar, IPAddress]
 
 
 class TimestampAbsoluteRelative(BaseModel):
