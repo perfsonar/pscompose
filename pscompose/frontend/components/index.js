a1d9dddc0c1b949f6f54423652647c1c24afb0bb
James Kafader <jkafader@es.net>
2026-03-02 23:59:49 -0800

    guess for the contents that should be in this file

---
+++ pscompose/frontend/components/index.js
@@ -0,0 +1,41 @@
+import "./ps-button.js";
+import "./ps-form-control.js";
+import "./ps-input-checkbox-star.js";
+import "./ps-input-checkbox.js";
+import "./ps-input-file.js";
+import "./ps-input-label.js";
+import "./ps-input-message.js";
+import "./ps-input-number.js";
+import "./ps-input-switch.js";
+import "./ps-input-text-autocomplete.js";
+import "./ps-input-text.js";
+import "./ps-modal.js";
+import "./ps-progress-bar.js";
+import "./ps-select-excludes.js";
+import "./ps-select-multi.js";
+import "./ps-select.js";
+import "./ps-textarea-json.js";
+import "./ps-textarea.js";
+import "./ps-tooltip.js";
+
+export default [
+    "ps-button",
+    "ps-form-control",
+    "ps-input-checkbox-star",
+    "ps-input-checkbox",
+    "ps-input-file",
+    "ps-input-label",
+    "ps-input-message",
+    "ps-input-number",
+    "ps-input-switch",
+    "ps-input-text-autocomplete",
+    "ps-input-text",
+    "ps-modal",
+    "ps-progress-bar",
+    "ps-select-excludes",
+    "ps-select-multi",
+    "ps-select",
+    "ps-textarea-json",
+    "ps-textarea",
+    "ps-tooltip",
+];
