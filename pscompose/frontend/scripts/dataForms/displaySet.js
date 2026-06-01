6ffffbe0c659addc3475b4637e87db69b96e946b
Johanna Lee <johyunjilee@gmail.com>
2026-06-01 12:14:47 -0400

    fix display set

--- pscompose/frontend/scripts/dataForms/displaySet.js
+++ pscompose/frontend/scripts/dataForms/displaySet.js
@@ -1,18 +1,13 @@
 async function taskReference() {
     const refField = document.getElementById("#/properties/reference");
-    const metaField = document.getElementById("#/properties/_meta");
+    let ref_data = refField.value || undefined;
 
     if (refField.parentElement.querySelector("ps-button[data-task-ref]")) return;
-
-    const ref_data =
-        typeof refField.value === "string" ? JSON.parse(refField.value) : refField.value;
-    const meta_data =
-        typeof metaField.value === "string" ? JSON.parse(metaField.value) : metaField.value;
 
     // Create ps-button
     const button = document.createElement("ps-button");
     button.label =
-        "display-task-group" in ref_data || "display-name" in meta_data
+        ref_data !== undefined && "display-task-group" in ref_data
             ? "Edit Display Set"
             : "Add Display Set";
     button.setAttribute("theme", "AddBtn");
@@ -28,19 +23,46 @@ async function taskReference() {
     modal.id = "task-modal";
     modal.setAttribute("question", button.label);
     modal.setAttribute("confirm-label", "Save");
+    modal.setAttribute("icon", "save");
+    modal.setAttribute("theme", "Success");
 
+    const setSource =
+        ref_data !== undefined && "display-set-source" in ref_data
+            ? JSON.stringify(ref_data["display-set-source"])
+            : JSON.stringify('{% jq .addresses[0]._meta."display-set" %}');
+    const setDest =
+        ref_data !== undefined && "display-set-dest" in ref_data
+            ? JSON.stringify(ref_data["display-set-dest"])
+            : JSON.stringify('{% jq .addresses[1]._meta."display-set" %}');
+    const taskName =
+        ref_data !== undefined && "display-task-name" in ref_data
+            ? JSON.stringify(ref_data["display-task-name"])
+            : "";
     const taskGroup =
-        "display-task-group" in ref_data ? JSON.stringify(ref_data["display-task-group"]) : "";
-    const taskName = "display-name" in meta_data ? JSON.stringify(meta_data["display-name"]) : "";
+        ref_data !== undefined && "display-task-group" in ref_data
+            ? JSON.stringify(ref_data["display-task-group"])
+            : "[]";
 
     const msg = `
     <div class="confirm-message">
         <form class='task-ref-form'>
             <ps-input-text 
+                id="display-set-source" 
+                label="display-set-source" 
+                value='${setSource}'>
+            </ps-input-text>
+            <ps-input-text 
+                id="display-set-dest" 
+                label="display-set-dest" 
+                value='${setDest}'>
+            </ps-input-text>
+            <ps-input-text 
+                id="display-task-name" 
                 label="display-task-name" 
                 value='${taskName}'>
             </ps-input-text>
             <ps-input-text-multi 
+                id="display-task-group" 
                 label="display-task-group" 
                 value='${taskGroup}'>
             </ps-input-text-multi>
@@ -56,20 +78,13 @@ async function taskReference() {
     refField.parentElement.appendChild(button);
 
     modal.addEventListener("confirm-yes-clicked", async function (event) {
-        // Query the form inside the modal's message
         const form = modal.querySelector(".task-ref-form");
 
-        ref_data["display-set-source"] =
-            ref_data["display-set-source"] || '{% jq .addresses[0]._meta."display-set" %}';
-        ref_data["display-set-dest"] =
-            ref_data["display-set-dest"] || '{% jq .addresses[1]._meta."display-set" %}';
-        ref_data["display-task-name"] =
-            ref_data["display-task-name"] || '{% jq .task._meta."display-name" %}';
-        ref_data["display-task-group"] = form.querySelector("ps-input-text-multi").value;
-
-        meta_data["display-name"] = form.querySelector("ps-input-text").value;
-        metaField.value = meta_data;
-        metaField.dispatchEvent(new Event("change", { bubbles: true }));
+        if (ref_data == undefined || typeof ref_data !== "object") ref_data = {};
+        ref_data["display-set-source"] = form.querySelector("#display-set-source").value;
+        ref_data["display-set-dest"] = form.querySelector("#display-set-dest").value;
+        ref_data["display-task-name"] = form.querySelector("#display-task-name").value;
+        ref_data["display-task-group"] = form.querySelector("#display-task-group").value;
 
         refField.value = ref_data;
         refField.dispatchEvent(new Event("change", { bubbles: true }));
@@ -78,14 +93,14 @@ async function taskReference() {
 
 async function addressMetaData() {
     const metaField = document.getElementById("#/properties/_meta");
-    const data = metaField.value;
+    let data = metaField.value || undefined;
 
     if (metaField.parentElement.querySelector("ps-button[data-address-meta]")) return;
 
     // Create ps-button
     const button = document.createElement("ps-button");
     button.label =
-        "display-name" in data || "display-set" in data ? "Edit Display Set" : "Add Display Set";
+        data !== undefined && "display-set" in data ? "Edit Display Set" : "Add Display Set";
     button.setAttribute("type", "button");
     button.setAttribute("theme", "AddBtn");
     button.setAttribute("lefticon", "plus");
@@ -97,12 +112,14 @@ async function addressMetaData() {
     // Create modal
     const modal = document.createElement("ps-modal");
     const displaySet =
-        "display-name" in data || "display-set" in data
+        data !== undefined && "display-set" in data
             ? JSON.stringify(data["display-set"] || data["display-name"])
             : "";
     modal.id = "address-modal";
     modal.setAttribute("question", button.label);
     modal.setAttribute("confirm-label", "Save");
+    modal.setAttribute("icon", "save");
+    modal.setAttribute("theme", "Success");
 
     const msg = `
     <div class="confirm-message">
@@ -120,6 +137,8 @@ async function addressMetaData() {
 
     modal.addEventListener("confirm-yes-clicked", async function () {
         const form = modal.querySelector(".address-meta-form");
+
+        if (data == undefined || typeof data !== "object") data = {};
         data["display-set"] = form.querySelector("ps-input-text").value;
         metaField.value = data;
         metaField.dispatchEvent(new Event("change", { bubbles: true }));
